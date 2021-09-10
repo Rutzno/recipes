3ad8e83622b06341840f8ffda3e3b660f3bf4ecc
Rutzno <d.rutzno@gmail.com>
2021-09-10 12:58:36 +0100

    Updated

--- src/main/java/com/diarpy/recipes/RecipesApplication.java
+++ src/main/java/com/diarpy/recipes/RecipesApplication.java
@@ -3,6 +3,12 @@ package com.diarpy.recipes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Mack_TB
+ * @version 1.0.7
+ * @since 8/16/2021
+ */
+
 @SpringBootApplication
 public class RecipesApplication {
 
