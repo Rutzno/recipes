a6860e20659fdf009ceec99c9bb599b8eeaadb10
Rutzno <d.rutzno@gmail.com>
2021-09-10 12:34:25 +0100

    Updated

--- src/main/java/com/diarpy/recipes/RecipeController.java
+++ src/main/java/com/diarpy/recipes/RecipeController.java
@@ -5,7 +5,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 @RestController
 public class RecipeController {
-    private Recipe recipe;
+    private Recipe recipe = new Recipe(
+            "Fresh Mint Tea",
+            "Light, aromatic and refreshing beverage, ...",
+            "boiled water, honey, fresh mint leaves",
+            "1) Boil water. 2) Pour boiling hot water into a mug. 3) Add fresh mint leaves. 4) Mix and let the mint leaves seep for 3-5 minutes. 5) Add honey and mix again."
+    );
 
     @PostMapping("/recipe")
     public void postRecipe(@RequestBody Recipe r) {
