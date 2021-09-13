cbe57952b9335f59952554434c8131b037a10d31
Rutzno <d.rutzno@gmail.com>
2021-09-13 19:45:21 +0100

    Stage 3

--- src/main/java/com/diarpy/recipes/presentation/RecipeController.java
+++ src/main/java/com/diarpy/recipes/presentation/RecipeController.java
@@ -1,37 +1,37 @@
 package com.diarpy.recipes.presentation;
 
 import com.diarpy.recipes.businessLayer.Recipe;
+import com.diarpy.recipes.businessLayer.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * @author Mack_TB
- * @version 1.0.7
- * @since 9/9/2021
- */
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class RecipeController {
-    private Map<Integer, Recipe> recipeMap = new HashMap<>();
+    final RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @PostMapping("/recipe/new")
-    public Object postRecipe(@RequestBody Recipe recipe) {
-        int id = recipeMap.size() + 1;
-        recipeMap.put(id, recipe);
-        return String.format("{\"id\": %d}", id);
+    public Object postRecipe(@Valid @RequestBody Recipe recipe) {
+        Recipe newRecipe = recipeService.save(recipe);
+        return String.format("{\"id\": %d}", newRecipe.getId());
     }
 
     @GetMapping("/recipe/{id}")
-    public Recipe getRecipe(@PathVariable int id) {
-        if (id > recipeMap.size()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                    "Recipe not found for id = " + id);
-        }
-        return recipeMap.get(id);
+    public Recipe getRecipe(@PathVariable long id) {
+        return recipeService.findRecipeById(id);
+    }
+
+    @DeleteMapping("/recipe/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteRecipe(@PathVariable long id) {
+        recipeService.deleteRecipeById(id);
     }
 }
