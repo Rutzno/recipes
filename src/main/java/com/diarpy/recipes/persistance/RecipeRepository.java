15a7e83c7b371e8cdb6439b5c51252b10fef6696
Rutzno <d.rutzno@gmail.com>
2021-09-13 19:59:06 +0100

    Stage 3 - adding author

--- src/main/java/com/diarpy/recipes/persistance/RecipeRepository.java
+++ src/main/java/com/diarpy/recipes/persistance/RecipeRepository.java
@@ -4,6 +4,12 @@ import com.diarpy.recipes.businessLayer.Recipe;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Mack_TB
+ * @version 1.0.7
+ * @since 9/9/2021
+ */
+
 @Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
