7d3302e5739ae572a4f3a60904b0f8c961227afb
Rutzno <d.rutzno@gmail.com>
2021-09-13 19:41:22 +0100

    Stage 3

--- src/main/java/com/diarpy/recipes/businessLayer/RecipeService.java
+++ src/main/java/com/diarpy/recipes/businessLayer/RecipeService.java
@@ -1,10 +1,10 @@
 package com.diarpy.recipes.businessLayer;
 
+import com.diarpy.recipes.persistance.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-import recipes.persistance.RecipeRepository;
 
 @Service
 public class RecipeService {
