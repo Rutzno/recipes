fc44879b1c48422df765c9a5fa3d7a5029414385
Rutzno <d.rutzno@gmail.com>
2021-09-14 13:35:20 +0100

    Stage 4

--- src/main/java/com/diarpy/recipes/persistance/RecipeRepository.java
+++ src/main/java/com/diarpy/recipes/persistance/RecipeRepository.java
@@ -4,6 +4,8 @@ import com.diarpy.recipes.businessLayer.Recipe;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author Mack_TB
  * @version 1.0.7
@@ -12,5 +14,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-
+    List<Recipe> findRecipeByCategoryOrderByDateDesc(String category);
+    List<Recipe> findRecipeByNameOrderByDateDesc(String name);
 }
