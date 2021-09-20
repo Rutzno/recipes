d2caac6bd2ccacdb6faa3024b85b473d8cf139f9
Rutzno <d.rutzno@gmail.com>
2021-09-20 12:29:12 +0100

    Stage 5

---
+++ src/main/java/com/diarpy/recipes/businessLayer/UserService.java
@@ -0,0 +1,43 @@
+package com.diarpy.recipes.businessLayer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import recipes.persistance.UserRepository;
+import recipes.persistance.UserRoleRepository;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private UserRoleRepository userRoleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       UserRoleRepository userRoleRepository,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.userRoleRepository = userRoleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register(User user) {
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "this email is already taken");
+        } else {
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+            user.setActive(true);
+            userRepository.save(user);
+
+            UserRole userRole = new UserRole();
+            userRole.setEmail(user.getEmail());
+            userRole.setRole("USER");
+            userRoleRepository.save(userRole);
+        }
+    }
+}
