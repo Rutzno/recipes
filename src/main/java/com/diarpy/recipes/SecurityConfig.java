6a8e27f71b6e3415a2ed28ec7de2075b7a8dd3d6
Rutzno <d.rutzno@gmail.com>
2021-09-20 12:44:23 +0100

    Stage 5

---
+++ src/main/java/com/diarpy/recipes/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.diarpy.recipes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery("SELECT username as principal, password as credentials, active FROM user WHERE username = ?")
+            .authoritiesByUsernameQuery("SELECT username as principal, role as role FROM user_role WHERE username = ?")
+            .rolePrefix("ROLE_")
+            .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
+        http.csrf().disable();
+        http.httpBasic()
+            .and()
+            .authorizeRequests()
+                .antMatchers("/api/register/**").permitAll()
+                .antMatchers("/api/recipe/**").authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
