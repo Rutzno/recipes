1483d3b060b7adf66c7bbcf28c7f598cda65da20
Rutzno <d.rutzno@gmail.com>
2021-09-20 12:27:06 +0100

    Stage 5

---
+++ src/main/java/com/diarpy/recipes/businessLayer/UserRole.java
@@ -0,0 +1,30 @@
+package com.diarpy.recipes.businessLayer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_role")
+public class UserRole implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Email is mandatory")
+    @Pattern(regexp = "\\w{3,}@[a-z]+\\.[a-z]{2,3}")
+    @Column(name = "username", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "role")
+    private String role;
+}
