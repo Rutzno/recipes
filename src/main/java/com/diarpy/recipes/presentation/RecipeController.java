8dbab717b83e8e83642d45d7a44b6033429bb0e8
Rutzno <d.rutzno@gmail.com>
2021-09-20 12:37:15 +0100

    Stage 5

--- src/main/java/com/diarpy/recipes/presentation/RecipeController.java
+++ src/main/java/com/diarpy/recipes/presentation/RecipeController.java
@@ -1,44 +1,36 @@
 package com.diarpy.recipes.presentation;
 
-import com.diarpy.recipes.businessLayer.Recipe;
-import com.diarpy.recipes.businessLayer.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import com.diarpy.recipes.businessLayer.Recipe;
+import com.diarpy.recipes.businessLayer.RecipeService;
+import com.diarpy.recipes.businessLayer.User;
+import com.diarpy.recipes.businessLayer.UserService;
+
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
-
-/**
- * @author Mack_TB
- * @version 1.0.7
- * @since 9/9/2021
- */
-
-/**
- * Exampe of recipe
- {
- "name": "Fresh Mint Tea",
- "category": "beverage",
- "description": "Light, aromatic and refreshing beverage, ...",
- "ingredients": ["boiled water", "honey", "fresh mint leaves"],
- "directions": ["Boil water", "Pour boiling hot water into a mug", "Add fresh mint leaves", "Mix and let the mint leaves seep for 3-5 minutes", "Add honey and mix again"]
- }
- */
 
 @RestController
 @RequestMapping("/api")
 public class RecipeController {
     final RecipeService recipeService;
+    final UserService userService;
 
     @Autowired
-    public RecipeController(RecipeService recipeService) {
+    public RecipeController(RecipeService recipeService,
+                            UserService userService) {
         this.recipeService = recipeService;
+        this.userService = userService;
     }
 
     @PostMapping("/recipe/new")
-    public Object postRecipe(@Valid @RequestBody Recipe recipe) {
-        Recipe newRecipe = recipeService.save(recipe);
+    public Object postRecipe(@Valid @RequestBody Recipe recipe, Authentication authentication) {
+        recipe.setDate(LocalDateTime.now());
+        Recipe newRecipe = recipeService.save(recipe, authentication);
         return String.format("{\"id\": %d}", newRecipe.getId());
     }
 
@@ -49,14 +41,14 @@ public class RecipeController {
 
     @DeleteMapping("/recipe/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void deleteRecipe(@PathVariable long id) {
-        recipeService.deleteRecipeById(id);
+    public void deleteRecipe(@PathVariable long id, Authentication authentication) {
+        recipeService.deleteRecipeById(id, authentication);
     }
 
     @PutMapping("/recipe/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void updateRecipe(@PathVariable long id, @Valid @RequestBody Recipe recipe) {
-        recipeService.updateRecipe(id, recipe);
+    public void updateRecipe(@PathVariable long id, @Valid @RequestBody Recipe recipe, Authentication authentication) {
+        recipeService.updateRecipe(id, recipe, authentication);
     }
 
     @GetMapping("/recipe/search/")
@@ -64,4 +56,8 @@ public class RecipeController {
         return recipeService.searchRecipesBy(allParams);
     }
 
+    @PostMapping(path = "/register")
+    public void register(@Valid @RequestBody User user) {
+        userService.register(user);
+    }
 }
