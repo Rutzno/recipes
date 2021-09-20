4343d41405cab94c5f945031e7472c4829627a9b
Rutzno <d.rutzno@gmail.com>
2021-09-20 13:00:46 +0100

    Stage 5

--- src/main/java/com/diarpy/recipes/persistance/UserRoleRepository.java
+++ src/main/java/com/diarpy/recipes/persistance/UserRoleRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.diarpy.recipes.businessLayer.UserRole;
 
+/**
+ * @author Mack_TB
+ * @version 1.0.7
+ * @since 9/9/2021
+ */
+
 @Repository
 public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
 
